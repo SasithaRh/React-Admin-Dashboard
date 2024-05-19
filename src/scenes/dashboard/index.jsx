2f4a3155fd6d64b11b529b94199ba45da646cd56
SasithaRh <ruvishansth1030@gmail.com>
2024-05-19 12:28:46 +0530

    Add theme provider

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      sadasdasdasd
+    </div>
+  )
+}
+
+export default index
---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div>
+      Top bar
+    </div>
+  )
+}
+
+export default Topbar
